ca5fdfb853aa189ba8c97ceae7dd5b32038cbab6
Fernando Borretti <eudoxiahp@gmail.com>
2013-11-15 09:18:41 -0200

    Moar tests

--- t/cl-yaml.lisp
+++ t/cl-yaml.lisp
@@ -14,7 +14,8 @@
     (parse "bar")
     (parse "[1,2,3]")
     (parse "[[a,b],[c,d]]")
-    (parse "{a : 1, b : 2, c : 3}")))
+    (parse "{a : 1, b : 2, c : 3}")
+    (parse "{first : [1,2,{a : 1, b : 2}], second: {fst : a, snd : b}}")))
 
 (test flat-int
   (is (equal (parse "[1,2,3]") (list 1 2 3))))
@@ -27,5 +28,12 @@
              (list (list "a" 1)
                    (list "b" 2)
                    (list "c" 3)))))
+
+(test flat-map
+  (is (equalp (parse "{a : 1, b : 2}")
+              (let ((map (make-hash-table :test #'equal)))
+                (setf (gethash "a" map) 1
+                      (gethash "b" map) 2)
+                map))))
 
 (run!)
