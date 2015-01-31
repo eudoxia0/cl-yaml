145c3ff26ee107ae11eaafda8bed549a786fcc2d
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-31 14:04:19 -0200

    Change order of tests

--- t/cl-yaml.lisp
+++ t/cl-yaml.lisp
@@ -3,6 +3,6 @@
   (:use :cl :fiveam))
 (in-package :cl-yaml-test)
 
-(run! 'cl-yaml-test.parser:parser)
 (run! 'cl-yaml-test.float:float)
 (run! 'cl-yaml-test.scalar:scalar)
+(run! 'cl-yaml-test.parser:parser)
