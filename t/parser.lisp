05b24c4dcc4edc3cc91fe60ae0a2f4f0c14a0c06
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-31 17:46:36 -0200

    Add more parser tests

--- t/parser.lisp
+++ t/parser.lisp
@@ -8,7 +8,50 @@
   :description "YAML parser tests.")
 (in-suite parser)
 
-(test lists
-  (is
-   (equal (yaml.parser:parse-string "[a, b, c]")
-          (list :document (list "a" "b" "c")))))
+(defmacro define-test-cases ((name) &rest pairs)
+  `(test ,name
+     ,@(loop for (string form) in pairs collecting
+         `(is (equal (getf (yaml.parser:parse-string ,string)
+                           :document)
+                     ,form)))))
+
+(define-test-cases (special-scalars)
+  ("[true, True, TRUE]"
+   (list t t t))
+  ("[false, False, FALSE]"
+   (list nil nil nil))
+  ("[null, Null, NULL, ~]"
+   (list nil nil nil nil)))
+
+(define-test-cases (integers)
+  ("1"
+   1)
+  ("123456"
+   123456)
+  ("0001"
+   1)
+  ("-2"
+   -2)
+  ("0o31" ;; The old OCT 31 = DEC 25 joke
+   25))
+
+(define-test-cases (floats)
+  ("6.62606957e-34"
+   6.62607e-34))
+
+(define-test-cases (lists)
+  ("[a, b, c]"
+   (list "a" "b" "c"))
+  ("[1, 2, 3]"
+   (list 1 2 3)))
+
+(define-test-cases (nested-lists)
+  ;; Right-nested list
+  ("[1, [2, [3]]]"
+   (list 1 (list 2 (list 3))))
+  ;; Left-nested list
+  ("[[[1], 2], 3]"
+   (list (list (list 1) 2) 3))
+  ;; Mid-centered list
+  ("[1, [2, [3], 4], 5]"
+   (list 1 (list 2 (list 3) 4) 5)))
